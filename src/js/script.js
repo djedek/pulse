c5b08808b0eacdf3eb83446733862b84b7ecc1a3
djedek <krivev10@gmail.com>
2020-10-02 19:15:37 +0300

    Маска ввода номера телефона, отправка писем с сайта

--- src/js/script.js
+++ src/js/script.js
@@ -75,6 +75,24 @@ $(document).ready(function(){
 	valideForms('#consultation-form');
 	valideForms('#consultation form');
 	valideForms('#order form');
+
+	$('input[name=phone]').mask("+9 (999) 999-99-99");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
 });
 
 const slider = tns({
