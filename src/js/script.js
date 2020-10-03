111291aea4d2b16627522ad137d921aeb522171a
djedek <krivev10@gmail.com>
2020-10-03 18:11:29 +0300

    Валидация сайта

--- src/js/script.js
+++ src/js/script.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
 			$(this).on('click', function(e){
 				e.preventDefault();
 				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+				$('.catalog-item__right').eq(i).toggleClass('catalog-item__right_active');
 			})
 		})
 	};
@@ -104,7 +104,7 @@ $(document).ready(function(){
 		}
 	});
 
-	$("a[href^='#']").click(function(){
+	$("a[href='#up'], a[href='#catalog']").click(function(){
 		const _href = $(this).attr("href");
 		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 		return false;
